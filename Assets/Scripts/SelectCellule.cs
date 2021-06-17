9539f157404212c5b518073605c5fbdb16ac191f
Meli83 <meli83@orange.fr>
2021-06-17 13:56:30 +0200

    Suppression des lignes de codes en commentaires

--- Assets/Scripts/SelectCellule.cs
+++ Assets/Scripts/SelectCellule.cs
@@ -1,32 +1,33 @@
 using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class SelectCellule : MonoBehaviour {
     public Camera camera;
     public Transform Cellfav;
-    //public Settings autreScriptSettings;
-void Update(){
-   //autreScriptSettings = GameObject.FindObjectOfType(typeof(Settings)) as Settings;
-   Settings settings = GameObject.Find("Settings").GetComponent<Settings>();
-   camera = settings.get_camera();
-
-   if (Input.GetMouseButtonDown(0)){ // if left button pressed
-        RaycastHit hit;
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        
-        if (Physics.Raycast(ray, out hit)) {
-            
-            Cellfav = hit.collider.transform;
-            //Surbrillance de l'objet
-
-            //Affichage de la description de la cellule
-            print("Coucou on m'a cliqué dessus");
-            foreach (var kvp in Cellfav.GetComponent<Metadata>().cell_data)
-    	        Debug.Log(kvp.Key + " : " + kvp.Value);
-            
-        }
-    }
     
+    
+void Update(){
+
+        Settings settings = GameObject.Find("Settings").GetComponent<Settings>();
+        camera = settings.get_camera();
+
+
+        if (Input.GetMouseButtonDown(0)){ // if left button pressed
+                RaycastHit hit;
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+                
+                if (Physics.Raycast(ray, out hit)) {
+                    
+                    Cellfav = hit.collider.transform;
+                    //Surbrillance de l'objet
+
+                    //Affichage de la description de la cellule
+                    print("Coucou on m'a cliqué dessus");
+                    foreach (var kvp in Cellfav.GetComponent<Metadata>().cell_data)
+                        Debug.Log(kvp.Key + " : " + kvp.Value);
+                    
+                }
+            }
 }
 
 }
